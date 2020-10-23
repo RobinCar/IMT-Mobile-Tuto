d6bd14920d7b66cc5463a89d765aaf65d97e98df
RobinCar <robin.carrez@outlook.fr>
2020-10-23 14:13:15 +0200

    Fin tuto 4

--- 04-hellocapacitor/www/app.js
+++ 04-hellocapacitor/www/app.js
@@ -1,20 +1,3 @@
-let imageUrl = null;
-
-async function takePicture() {
-    const image = await capacitorExports.Camera.getPhoto({
-        quality: 90,
-        allowEditing: true,
-        resultType: capacitorExports.CameraResultType.Uri
-    });
-    // image.webPath will contain a path that can be set as an image src.
-    // You can access the original file using image.path, which can be
-    // passed to the Filesystem API to read the raw data of the image,
-    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
-    imageUrl = image.base64String;
-
-    presentModal(image.base64String);
-}
-
 const DEV_FEST_IMAGES_URL = "https://devfest2018.gdgnantes.com/";
 const DEV_FEST_GET_URL = "https://devfest-nantes-2018-api.cleverapps.io/blog";
 
@@ -24,30 +7,41 @@ fetch(DEV_FEST_GET_URL)
     })
     .then(function (data) {
         console.log(data);
-        appendData(data);
+        for (let i = 0; i < data.length; i++) {
+            createCard(data[i].title, data[i].brief, DEV_FEST_IMAGES_URL + data[i].image);
+            getLocalStorage();
+        }
     })
     .catch(function (err) {
         console.log(err);
-    })
+    });
 
-
-function appendData(json) {
+function createCard(titre, description, image) {
     let mainContainer = document.getElementById("#data");
-    for (let i = 0; i < json.length; i++) {
-        let card = document.createElement("ion-card");
-        let img = document.createElement("img");
-        img.src = DEV_FEST_IMAGES_URL + json[i].image;
-        let cardHeader = document.createElement("ion-card-header");
-        let cardTitle = document.createElement("ion-card-title");
-        let cardContent = document.createElement("ion-card-content");
-        cardTitle.innerHTML = json[i].title;
-        cardContent.innerHTML = json[i].brief;
-        cardHeader.appendChild(cardTitle);
-        card.appendChild(img);
-        card.appendChild(cardHeader);
-        card.appendChild(cardContent);
-        mainContainer.appendChild(card);
-    }
+    let card = document.createElement("ion-card");
+    card.id = titre;
+    let img = document.createElement("img");
+    img.src = image;
+    let cardHeader = document.createElement("ion-card-header");
+    let cardTitle = document.createElement("ion-card-title");
+    let cardContent = document.createElement("ion-card-content");
+    cardTitle.innerHTML = titre;
+    cardContent.innerHTML = description;
+    cardHeader.appendChild(cardTitle);
+    card.appendChild(img);
+    card.appendChild(cardHeader);
+    card.appendChild(cardContent);
+    mainContainer.appendChild(card);
+}
+
+async function takePicture() {
+    const image = await capacitorExports.Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: capacitorExports.CameraResultType.base64String
+    });
+
+    presentModal(image.base64String);
 }
 
 customElements.define('modal-content', class ModalContent extends HTMLElement {
@@ -61,55 +55,26 @@ customElements.define('modal-content', class ModalContent extends HTMLElement {
             </ion-buttons>
           </ion-toolbar>
         </ion-header>
-        <ion-content fullscreen>
+        <ion-content>
             <form name="image" [formGroup]="todo">
-            <ion-item>
-                <ion-label>Titre</ion-label>
-                <ion-input name="titre" required></ion-input>
-            </ion-item>
-            <ion-item>
-                <ion-label>Description</ion-label>
-                <ion-input name="description"></ion-input>
-            </ion-item>
-            <ion-button expand="block" [disabled]="!todo.valid" onClick="newImageForm()">Enregistrer</ion-button>
+                <ion-item>
+                    <ion-label>Titre</ion-label>
+                    <ion-input name="titre" required></ion-input>
+                </ion-item>
+                <ion-item>
+                    <ion-label>Description</ion-label>
+                    <ion-input name="description"></ion-input>
+                </ion-item>
+                <ion-button expand="block" [disabled]="!todo.valid" onClick="newImageForm()">Enregistrer</ion-button>
             </form>
         </ion-content>
       `;
     }
 });
 
-async function newImageForm() {
-    console.log("Creation nouvelle image");
-
-    let form = document.forms.image;
-    let mainContainer = document.getElementById("#data");
-
-    let card = document.createElement("ion-card");
-    let img = document.createElement("img");
-    img.src = document.querySelector('ion-modal').componentProps.image;
-    let cardHeader = document.createElement("ion-card-header");
-    let cardTitle = document.createElement("ion-card-title");
-    let cardContent = document.createElement("ion-card-content");
-    cardTitle.innerHTML = await form.elements.titre.value;
-    cardContent.innerHTML = await form.elements.description.value;
-    cardHeader.appendChild(cardTitle);
-    card.appendChild(img);
-    card.appendChild(cardHeader);
-    card.appendChild(cardContent);
-    mainContainer.appendChild(card);
-
-    dismissModal();
-}
-
-/**
- * presentModal displays the modal
- * 
- * @param {string} image
- */
 function presentModal(image) {
-    // create the modal with the `modal-page` component
     const modalElement = document.createElement('ion-modal');
-    modalElement.component = 'modal-page';
+    modalElement.component = 'modal-content';
     modalElement.cssClass = 'my-custom-class';
     modalElement.componentProps = {
         'image': image
@@ -120,13 +85,44 @@ function presentModal(image) {
     return modalElement.present();
 }
 
-/**
- * dismissModal closes the modal
- * 
- */
 async function dismissModal() {
     let modal = document.querySelector('ion-modal');
     await modal.dismiss({
         'dismissed': true
     });
+}
+
+async function newImageForm() {
+    console.log("Creation nouvelle image");
+
+    let form = document.forms.image;
+    let titre = await form.elements.titre.value;
+    let description = await form.elements.description.value;
+
+    addToLocalStorage(titre, description, document.querySelector('ion-modal').componentProps.image).then(function () {
+        getLocalStorage();
+        dismissModal();
+    });
+}
+
+async function addToLocalStorage(title, description, image) {
+    await capacitorExports.Storage.set({
+        key: title,
+        value: JSON.stringify({
+            'title': title,
+            'brief': description,
+            'image': image
+        })
+    });
+}
+
+async function getLocalStorage() {
+    let { keys } = await capacitorExports.Storage.keys();
+    for (let i = 0; i < keys.length; i++) {
+        let response = await capacitorExports.Storage.get({ key: keys[i] });
+        let json = JSON.parse(response.value);
+        if (!document.getElementById(json.title)) {
+            createCard(json.title, json.brief, "data:image/png;base64," + json.image);
+        }
+    }
 }
